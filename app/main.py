d71fcc83c75c73874f30a99a25745ccca151e65f
Dmytro Kravchenko <kravchenko.d@ajax.systems>
2022-10-17 18:21:21 +0300

    qwe

--- app/main.py
+++ app/main.py
@@ -1,23 +1,21 @@
 from flask import Flask, request, redirect
-import telebot
+import requests
 
 app = Flask(__name__)
 
 token = '5690431214:AAGo6k6QETMz9mApzuMGZiYU03Bkfp4iywA'
-
 chat_id = '-661936876'
-
-bot = telebot.TeleBot(token)
-
 
 @app.route('/api/send_form_data', methods=['POST'])
 def send_form_data():
     data = request.form
-    bot.send_message(chat_id, 'SITE: bond\n\n' + '\n'.join([f'{k} - {v}' for k, v in data.items()]))
+    text = 'SITE: bond\n\n' + '\n'.join([f'{k} - {v}' for k, v in data.items()])
+    requests.get(f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
     return redirect('/Thank-you.html', code=302)
 
 @app.route('/api/send_form_data_canberra', methods=['POST'])
 def send_form_data_canberra():
     data = request.form
-    bot.send_message(chat_id, 'SITE: canberra\n\n' + '\n'.join([f'{k} - {v}' for k, v in data.items()]))
+    text = 'SITE: canberra\n\n' + '\n'.join([f'{k} - {v}' for k, v in data.items()])
+    requests.get(f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
     return redirect('/', code=302)
